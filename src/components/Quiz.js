50a6918be4cb30dad590afd3ba17406cdac06537
GustMangust <gust.mangust@gmail.com>
2022-08-30 04:05:56 +0300

    feat: initial commit

---
+++ src/components/Quiz.js
@@ -0,0 +1,34 @@
+import Question from "./Question";
+import { useReducer, useState } from "react";
+
+const initialState = {
+    questions:[],
+    currentQuestionIndex: 0
+};
+
+const reducer = (state,action) =>{
+    console.log("reducer", state,action);
+    if(action.type === "NEXT_QUESTION"){
+        return {
+            ...state,
+            currentQuestionIndex: state.currentQuestionIndex+1
+        }
+    }
+    return state;
+}
+
+const Quiz = () => {
+    const [state, dispatch] = useReducer(reducer,initialState);
+    console.log(state);
+    return (
+        <div className="quiz">
+            <div>
+                <div className="score">Question 1/8</div>
+                <Question/>
+                <div className="next-button" onClick={()=> dispatch({type:"NEXT_QUESTION"})}>Next Question</div>
+            </div>
+        </div>
+    );     
+};
+
+export default Quiz;
